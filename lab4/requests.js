1799c16935cde18ae057d52590c6956d1346405d
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-12-20 22:32:27 +0200

    add requests

---
+++ lab4/requests.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+
+const userData = {
+    "email": "anastasiia.trembach@gmail.com",
+    "user_metadata": {},
+    "blocked": false,
+    "email_verified": false,
+    "app_metadata": {},
+    "given_name": "Anastasiia",
+    "family_name": "Trembach",
+    "name": "Anastasiia Trembach",
+    "nickname": "NASA",
+    "picture": "https://secure.gravatar.com/avatar/15626c5e0c749cb912f9d1ad48dba440?s=480&r=pg&d=https%3A%2F%2Fssl.gstatic.com%2Fs2%2Fprofiles%2Fimages%2Fsilhouette80.png",
+    "user_id": "nasa",
+    "connection": "Username-Password-Authentication",
+    "password": "Anas2712",
+    "verify_email": false
+}
+
+const tokenOptions = () => ({
+    method: 'POST',
+    url: `${process.env.MY_URL}/oauth/token`,
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    form:
+    {
+        client_id: process.env.KPI_CLIENT_ID,
+        client_secret: process.env.KPI_CLIENT_SECRET,
+        audience: `${process.env.MY_URL}/api/v2/`,
+        grant_type: 'client_credentials'
+    }
+});
+
+const createUserOptions = (token) => ({
+    method: 'POST',
+    url: `${process.env.MY_URL}/api/v2/users`,
+    headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(user)
+});
+
+const userTokenOptions = () => ({
+    method: 'POST',
+    url: `${process.env.MY_URL}/oauth/token`,
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+        grant_type: 'http://auth0.com/oauth/grant-type/password-realm',
+        username: userData.email,
+        password: userData.password,
+        audience: `${process.env.MY_URL}/api/v2/`,
+        scope: 'offline_access',
+        realm: 'Username-Password-Authentication',
+        client_id: process.env.KPI_CLIENT_ID,
+        client_secret: process.env.KPI_CLIENT_SECRET
+    })
+});
+
+const refreshTokenOptions = (refresh_token) => ({
+    method: 'POST',
+    url: `${process.env.MY_URL}/oauth/token`,
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    form:
+    {
+        grant_type: 'refresh_token',
+        client_id: process.env.KPI_CLIENT_ID,
+        client_secret: process.env.KPI_CLIENT_SECRET,
+        refresh_token: refresh_token
+    }
+});
+
+module.exports = {
+    tokenOptions,
+    createUserOptions,
+    userTokenOptions,
+    refreshTokenOptions
+}
